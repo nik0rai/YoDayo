4a478e22e6e7330824a46a710b2a9d828f0a6e3a
nik0rai <nikolay-sviridov@inbox.ru>
2021-11-26 20:47:07 +0300

    using delegate

--- 3.txt/6)/Prog.cs
+++ 3.txt/6)/Prog.cs
@@ -4,19 +4,46 @@ using System.Threading;
 
 namespace ConsoleApp5
 {
+    public delegate void KeyPressEventDelegate();
+    class KeyLogger
+    {
+        public event KeyPressEventDelegate DigitKeyPressed;
+        public event KeyPressEventDelegate AnyKeyPressed;
+
+        public void DigitKeyPressedInvoke() => DigitKeyPressed?.Invoke();
+        public void AnyKeyPressedInvoke() => AnyKeyPressed?.Invoke();
+    }
+    class Subscribers
+    {
+        private static ConsoleKey key;
+        public static void DigitPressed()
+        {
+            while (true)
+            {
+                key = Console.ReadKey(true).Key;
+                if (char.IsDigit((char)key))
+                    Run();
+            }
+        }
+        public static void AnyKeyPressed()
+        {
+            Run();
+        }
+        private static void Run()
+        {
+            using QuedTask quedTask = new QuedTask();
+            for (int i = 0; i < 3; i++) quedTask.Task($"Task {i}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            ConsoleKey cs = Console.ReadKey().Key;
-            Console.WriteLine();
+            KeyLogger keyLog = new KeyLogger();
 
-            if(cs is ConsoleKey)
-            {
-                using QuedTask quedTask = new();
-                for (int i = 0; i < 3; i++) quedTask.Task($"Task {i}");
-            }             
-            
-        }        
+            keyLog.DigitKeyPressed += Subscribers.DigitPressed;
+            keyLog.AnyKeyPressed += Subscribers.AnyKeyPressed;
+            keyLog.AnyKeyPressedInvoke();
+        }
     }
 }
